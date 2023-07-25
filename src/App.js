985ce3a18d5165bb4f82a3f7bfb0417b6e87b127
Abadie Eliott <eliott.abadiesouchar@icloud.com>
2023-07-25 20:53:14 +0200

    fix error at home page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/*" element={<Home/>} />
         <Route path="/about" element={<About/>}/>
         <Route path="/:id" element={<Logement/>}/>
         <Route path="/*" element={<Error404/>}/>
