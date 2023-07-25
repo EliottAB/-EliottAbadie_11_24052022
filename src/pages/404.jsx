007cf0ce687c9cef88f787a2c350ddb7bd8ca1cd
Abadie Eliott <eliott.abadiesouchar@icloud.com>
2023-07-25 21:07:04 +0200

    Push Test

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -8,7 +8,7 @@ export function Error404(){
     return <React.Fragment>
     <Header/> 
         <main className="main404">
-            <h1>404</h1>
+            <h1>404BIS</h1>
             <p><span>Oups! La page que</span> vous demandez n'existe pas.</p>
             <Link className="goback" to="/">Retourner sur la page d’accueil</Link>
         </main>
